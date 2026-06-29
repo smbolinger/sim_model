fe812893982ffdc8d70d80f361ada7d10186a946
Sarah <sarah.bolinger@gmail.com>
2026-06-29 09:19:37 -0500

    add Optimized class to separate value/gradient while sharing computational load

--- matlab_func.py
+++ matlab_func.py
@@ -31,41 +31,71 @@ def jrandom(rng_key,K,scl_fac):
   ret = jax.random.uniform(subkey,shape=K,minval=minv,maxval=maxv)
   return ret
 
+@jax.jit
+def ll_valgrad(x,obs): ## obs is passed from w/in the Class instance
+  val, grad = jax.value_and_grad(jPolyLike)(x,obs) 
+  return val, grad
+
+class Optimized:
+  ## should allow to make calculations once and then reference from
+  ## the two separate functions for value and gradient?
+  def __init__(self, obs):
+    self.obs = obs
+    self.cached_x = None
+    self.cached_val = None
+    self.cached_grad = None
+
+  # def jplCompute(self,pZero,obs):
+  def jplCompute(self,pZero):
+    pZero = np.asarray(pZero, dtype=np.float64).flatten()
+    if self.cached_x is not None and np.array_equal(pZero, self.cached_x):
+            return
+    # loss, grad = ll_valgrad(pZero,obs)
+    loss, grad = ll_valgrad(pZero,self.obs)
+    val = float(loss)
+    grad = np.array(grad,dtype=np.float64)
+    self.cached_x = np.copy(pZero)
+    self.cached_val = val
+    self.cached_grad = grad
+
+  def objective(self,pZero):
+    # self.jplCompute(pZero,obs)
+    self.jplCompute(pZero)
+    return self.cached_val
+
+  def gradient(self,pZero):
+    # self.jplCompute(pZero,obs)
+    self.jplCompute(pZero)
+    return self.cached_grad
+
 # def PolyMort(obsData,survey,config,useJax=False,scl_fac=0.1,plt=True,suff=""):
-def PolyMort(obsData,survey,config,useJax=True,scl_fac=0.1,plt=False,suff=""):
+def PolyMort(obsData,survey,config,useJax=True,scl_fac=0.05,plt=False,suff=""):
 # def PolyMort(obsData,survey,config,useJax=False,scl_fac=0.05):
-# def PolyMort(obsData,survey,config,useJax=True):
-  # K = 2
+
   obs = mk_obs_mat(obsData,survey,config)
   K = len(np.unique(obs[:,2])) - 1 ## number of different fates minus 1
   gtolr = 1e-8
 
   if useJax:
-    ## better to convert to jax w/in a wrapper function bc scipy takees numpy arrays, not jax
-    # pZero = 0.1 * jrandom(rng_key,K)
-    # pZero = 0.05 * jrandom(rng_key,K)
+    ## better to convert to jax w/in wrapper bc scipy takes np arrays, not jax
     # pZero = jrandom(rng_key,K,scl_fac)
     # s0 = 1 - jnp.sum(pZero)
-    fun1 = jPolyLike
-    ll_valgrad = jax.value_and_grad(fun1) ## value & gradient function
-    ll_obj =  functools.partial(fun1,obs=obs,K=K,sclf=scl_fac)
-    fun = ll_valgrad
-    # fun = jPolyLike
-    jaco = True if fun==ll_valgrad else False
+
+    # fun = jplWrapper
+    # jaco = True 
+    prob = Optimized(obs=obs)
+    fun = prob.objective
+    jaco = prob.gradient
 
   else:
-    # pZero = 0.1 * rng.uniform(0,1,size=(K))
-    # pZero = 0.05 * rng.uniform(0,1,size=(K))
     # pZero = rng.uniform(0.01,1,size=(K)) # print(f"untransformed {pZero=}")
     # pZero = rng.uniform(low=0.1,high=0.9,size=(K)) # print(f"untransformed {pZero=}")
-    # s0 = 1 - np.sum(pZero)
-    # m0 = rng.uniform(0,0.4) m1 = rng.uniform(0,0.2)
+    # s0 = 1 - np.sum(pZero) m0 = rng.uniform(0,0.4) m1 = rng.uniform(0,0.2)
     # pZero = 0.1 * np.array([m0,m1])
     fun = PolyLikelihood
     ll_obj =  functools.partial(fun,obs=obs,K=K,sclf=scl_fac)
     # jaco = ndt.Jacobian(lambda x: fun(x,obs))
-    jaco=False
-    # jaco = jax.grad(fun)
+    jaco=False # jaco = jax.grad(fun)
 
   pZero = rng.uniform(low=0.1,high=0.9,size=(K)) # print(f"untransformed {pZero=}")
   s0 = 1 - np.sum(pZero)
@@ -75,15 +105,24 @@ def PolyMort(obsData,survey,config,useJax=True,scl_fac=0.1,plt=False,suff=""):
 
   # bnd = [(0.00001,0.1),(0.00001,0.1)] bnd = np.repeat((0.00001,0.1),K)
   # bnd = (0.00001,0.1)
-  bnd = (0.00001,1)
-  bnd = [bnd] * K
+  lb = 0.00001
+  ub = 1.0
+  # lb = [lb]*K
+  # ub = [ub]*K
+  # bnd = optimize.Bounds([lb]*K,[ub]*K)
+  bnd = optimize.Bounds(lb,ub)
+  # bnd = (0.00001,1)
+  # bnd = [bnd] * K
   met = config.optimizer
-  arg = (obs,K,scl_fac)
+  # arg = (obs,K,scl_fac,)
+  ## NOTE specifying scl_fac inside of jPolyLike insteead of as arg
+  arg = (obs,)
   # print(f">> PolyMort: run optimizer - {met=} {pZero=} {fun=} {jaco=} {bnd=}")
   if plt: plot_jac(fun,arg,suff)
 
-  print(f">> PolyMort: run optimizer - {met=} {gtolr=}; untransformed {pZero=} {fun=} {bnd=}")
+  print(f">> PolyMort: run optimizer - {met=} {gtolr=}; untransformed {pZero=}\n{fun=} {bnd=}")
 
+  ## NOTE can use keep_feasible w/trust-constr to stay w/in bounds throughout
   out = optimize.minimize(fun,
                           pZero,
                           args=arg,
@@ -93,63 +132,69 @@ def PolyMort(obsData,survey,config,useJax=True,scl_fac=0.1,plt=False,suff=""):
                           # hess=ndt.Hessian(lambda x: fun(x,obs)),
                           bounds = bnd, # constraints = con,
                           # options={'gtol':1e-12, 'disp':True},
-                          options={'gtol':gtolr, 'disp':True},
-                          # options={'disp':True},
+                          # options={'gtol':gtolr, 'disp':True},
+                          options={'disp':True},
                  )
 
   print(f">> PolyMort: {out.success=} {out.message=} {out.nit=} {out.nfev=}")
-  # hess = ndt.Hessian(PolyLikelihood)(out.x,obs)
   ans = out.x * scl_fac
   s = 1-sum(ans) ## one minus sum of fitted values
   print(f"*** PolyMort: {ans.dtype=} {ans.shape=} {ans=} {s=} ",end=" ")
 
-  # hess = ndt.Hessian(ll_obj,method="complex")(out.x) # hess2 = approx_hess2(out.x, ll_obj)
   hess = ndt.Hessian(ll_obj,method="complex")(ans) # hess2 = approx_hess2(out.x, ll_obj)
   # hess = ndt.Hessian(ll_obj,step=1e-12)(ans) # hess2 = approx_hess2(out.x, ll_obj)
-  # hess = ndt.Hessian(ll_obj)(ans) # hess2 = approx_hess2(out.x, ll_obj)
-  # hess = jax.hessian(fun)(ans,obs,mat)
   se = np.sqrt(np.diag(np.linalg.inv(hess))) * scl_fac
   seS = np.sqrt(np.sum(np.sum(np.linalg.inv(hess)))) * scl_fac
   print(f"{se=} {seS=}",end=" ")
   print(f" {hess.flatten()=}")
 
-  # return (s, out.x, se, seS)
   return (s, seS, ans[0], se[0])
 
 # def mk_obs_mat(obsData, expo,exp1,config):
-def jPolyLike(pZero,obs,K,sclf):
+# def jPolyLike(pZero,obs,K,sclf):
+def jPolyLike(pZero,obs):
   # K=2
   nObs = obs.shape[0]
-  pZero = sclf*pZero
+  # pZero = sclf*pZero
+  ## NOTE: make sure this scaling constant matches scl_fac in PolyMort
+  pZero = 0.05*pZero
+  # print(f"{pZero=}")
 
   NLL = 0 ## initial NLL value
   s0 = 1 - jnp.sum(pZero)
   arr = [[s0,0,0],[pZero[0],1,0],[pZero[1],0,1]]
   M = jnp.array(arr,dtype=jnp.float64) # print(f"\t\t{mat=}") 
   # mat = jnp.array(arr,dtype=jnp.float64)
-  print(f"\t\t {type(M)=} {M.dtype=} {M.shape=}\n{M=}")
-  d = obs[:,1]
-  fate = obs[:,2]
-  print(f"jPolyLike: {type(d)=} {d.dtype=} {np.unique(d,return_counts=True)=}"
-        f"\n\t {type(fate)=} {fate.dtype=} {np.unique(fate,return_counts=True)=}")
+  if config.debugLL>=4: print(f"\t {type(M)=} {M.dtype=} {M.shape=}\n{M.flatten()=}")
+  # d = obs[:,1]
+  # fate = obs[:,2]
+  # print(f"jPolyLike: {type(d)=} {d.dtype=} {np.unique(d,return_counts=True)=}"
+        # f"\n\t {type(fate)=} {fate.dtype=} {np.unique(fate,return_counts=True)=}")
 
   ## the problem is still that the matrix power cannot easily be vectorized
   for n in jnp.arange(nObs):
   # for n in range(nObs):
   # for n in obsStep:
-    # print(f"nest ID: {obs[n,0]}",end=" ")
-    # d = obs[n,1] fate = obs[n,2]
+    ## will print when atype==small2 (which makes debugLL=4)
+    if config.debugLL>=4: print(f"\t\tnest ID: {obs[n,0]}",end=" ")
+    d = obs[n,1]
+    fate = obs[n,2]
     # print(f" {fate[n]=} {d[n]=}")
-    M_to_the_d = jnp.linalg.matrix_power(M,d[n]) 
-    # M_to_the_d = jnp.linalg.matrix_power(M,d) 
+    if config.debugLL>=4: print(f" {fate=} {d=}")
+    # M_to_the_d = jnp.linalg.matrix_power(M,d[n]) 
+    M_to_the_d = jnp.linalg.matrix_power(M,d) 
     # print(f" {M_to_the_d=}",end=" ")
-    # L = M_to_the_d[fate,0] L = M_to_the_d[fate-1,0]
+    L = M_to_the_d[fate,0] # L = M_to_the_d[fate-1,0]
     # L = M_to_the_d[fate[n]-1,0]
-    L = M_to_the_d[fate[n],0] # print(f" M_to_the_d[fate-1,0] {L=}")
+    # L = M_to_the_d[fate[n],0]
+    if config.debugLL>=4: print(f"\t\tM_to_the_d[fate-1,0] {-jnp.log(L)=} {L=}")
     NLL = NLL - jnp.log(L)
 
-  print(f"jPolyLike: {NLL=} {type(NLL)=}")
+  if config.debugLL>=4: print(f"\tjPolyLike: {NLL=} {type(NLL)=}")
   return NLL
+
+# ll_valgrad = jax.value_and_grad(jPolyLike) ## value & gradient function
+#   return out, jax.grad(lambda x
 
 def mk_obs_mat(obsData,survey,config,exp1=False):
   if isinstance(obsData, pd.DataFrame):
@@ -195,7 +240,6 @@ def mk_obs_mat(obsData,survey,config,exp1=False):
 
   return out
 
-# def PolyLikelihood(pZero,obs,nObs,obsStep):
 # def PolyLikelihood(pZero,obs,M):
 def PolyLikelihood(pZero,obs,K,sclf):
   # K = 2
@@ -222,25 +266,19 @@ def PolyLikelihood(pZero,obs,K,sclf):
   # if config.debugLL>=4: print(f"\t>> PolyLikelihood: {NLL=}")
   return NLL
 
-# def plot_jac(fun,obs,suff=""):
-def plot_jac(fun,arg,suff=""):
-  beta1 = jnp.linspace(0,1,21)
-  beta2 = jnp.linspace(0,1,21)
-  beta = jnp.array([beta1,beta2])
-  print(f">> {beta=}")
-  print(">> creating jax gradient",end=" ")
-  ll_grad = jax.grad(fun,argnums=0) ## vectorized gradient function
-  print(">> creating mapped jax gradient",end=" ")
-  ll_vecgrad = jax.vmap(ll_grad, in_axes=(0,None,None,None)) ## vectorized gradient function
-  print(">> making plot",end=" ")
-  # fig, (ax1, ax2) = plt.subplots(2, sharex=True, figsize=(12, 8))
-  plt.plot(beta,ll_vecgrad(beta,*arg))
-  # ax1.plot(beta,ll_vecgrad(beta,obs), lw=2)
-  print(">> adding axis",end=" ")
-  # ax1.plot(beta,ll_vecgrad(beta,*arg), lw=2)
-  print(">> saving plot to file")
-  # ax2.plot(beta,ll_obj(beta), lw=2)
-  plt.savefig(f"figs/jac{suff}.png")
+# def jplWrapper(pZero,obs,K,sclf):
+def jplWrapper(pZero,obs):
+  jpZero = jnp.array(pZero)
+  # loss, grad = ll_valgrad(pZero,obs,K,sclf)
+  loss, grad = ll_valgrad(jpZero,obs)
+  val = float(loss)
+  grad = np.array(grad,dtype=np.float64)
+  print(f"{val=} {type(val)=}")
+  print(f"{grad=} {type(grad)=}")
+  # return val, grad
+  # return val, grad
+  return float(loss), np.array(grad)
+
 
 def j_obs_mat(obsData,survey,config,exp1=False):
   if isinstance(obsData, pd.DataFrame):
@@ -275,6 +313,26 @@ def j_obs_mat(obsData,survey,config,exp1=False):
 
   out = jnp.column_stack([IDcol,expos,fatecol])
   print(f"\t\tmk_obs_mat: {np.isnan(out).sum(axis=0)=}\n {out[0:8,:]=}")
+
+# def plot_jac(fun,obs,suff=""):
+def plot_jac(fun,arg,suff=""):
+  beta1 = jnp.linspace(0,1,21)
+  beta2 = jnp.linspace(0,1,21)
+  beta = jnp.array([beta1,beta2])
+  print(f">> {beta=}")
+  print(">> creating jax gradient",end=" ")
+  ll_grad = jax.grad(fun,argnums=0) ## vectorized gradient function
+  print(">> creating mapped jax gradient",end=" ")
+  ll_vecgrad = jax.vmap(ll_grad, in_axes=(0,None,None,None)) ## vectorized gradient function
+  print(">> making plot",end=" ")
+  # fig, (ax1, ax2) = plt.subplots(2, sharex=True, figsize=(12, 8))
+  plt.plot(beta,ll_vecgrad(beta,*arg))
+  # ax1.plot(beta,ll_vecgrad(beta,obs), lw=2)
+  print(">> adding axis",end=" ")
+  # ax1.plot(beta,ll_vecgrad(beta,*arg), lw=2)
+  print(">> saving plot to file")
+  # ax2.plot(beta,ll_obj(beta), lw=2)
+  plt.savefig(f"figs/jac{suff}.png")
 
 def jPolyMort(obsData,survey,config):
   K=2
