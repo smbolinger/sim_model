57b157e99c4ba91fb9e1af79546a2d99dd1971c9
Sarah <sarah.bolinger@gmail.com>
2026-07-01 14:01:44 -0500

    added padded arrays and othr changes to keep jax from re-compiling every time

--- matlab_func.py
+++ matlab_func.py
@@ -12,7 +12,7 @@ import jax.numpy as jnp
 from dsrCalc import calc_daily_expo
 from helpers import print
 from print_func import dfPrint
-from rsettings import config, odir
+from rsettings import config, odir, atype
 from scipy import optimize
 # from jax.scipy.optimize import minimize
 # from jaxopt import ScipyBoundedMinimize
@@ -25,8 +25,9 @@ np.set_printoptions(suppress=True,precision=7)
 from jax import config as jconfig
 jconfig.update("jax_enable_x64", True)
 # if config.testing=="yes":
-#   jconfig.update("jax_log_compiles", True)
-#   jconfig.update("jax_explain_cache_misses", True)
+if atype=="norm":
+  jconfig.update("jax_log_compiles", True)
+  jconfig.update("jax_explain_cache_misses", True)
 
 def jrandom(rng_key,K,scl_fac):
   minv = scl_fac
@@ -38,83 +39,48 @@ def jrandom(rng_key,K,scl_fac):
 
 
 def nll_lookup(M, dVal, fateVal):
-# def nll_lookup(Mpower, dVal, fateVal):
   ## NOTE called from w/in jPolyLike
   Mpower = [jnp.eye(3), M]
-  # for _ in range(9):
-    # Mpower.append(Mpower[-1] @ M)
-  Mpower = [Mpower[-1]@M for _ in range(9)]
+  for _ in range(9):
+    Mpower.append(Mpower[-1] @ M)
+  ## violates jax rule to keep shape static:
+  # dVal = dVal[dVal>0] fateVal = fateVal[dVal>0]
+  # Mpower = [Mpower[-1]@M for _ in range(9)]
   mPowStack = jnp.stack(Mpower)
   mPowMatch = mPowStack[dVal][:,:,0].flatten()
   # print(f"\t\t{mPowStack=} {mPowMatch=}")
   mPowInd = jnp.arange(len(dVal)) * 3
   newInd = mPowInd + fateVal
   mPowFate = mPowMatch[newInd]
+  
   # print(f"\t\t{newInd=} {mPowFate=}")
-
+  if atype=="norm":
+    print(
+        f"\t\t{len(dVal)=} {len(fateVal)=}\n{len(M)}"
+        f"\t\t{len(mPowStack)=} {len(mPowMatch)=} {len(mPowInd)=}"
+        f"\t\t{len(newInd)=} {len(mPowFate)=}\n{mPowFate=}") ## these vals should stay constant
+  ## need mPowFate at padded locations to == 1 so log is 0
   return -jnp.sum(jnp.log(mPowFate))
   
-# def jPolyLike(pZero,obs,nObs):
-# def jPolyLike(pZero,obs,nObs,scl_fac):
-# def jPolyLike(pZero,obs,nObs):
 ## things that rely on pZero need to be created in this function
 ## not the wrapper, so we can call hessian with same args
-# def jPolyLike(Mpower,dVal,fateVal):
-# @jax.jit
+@jax.jit
 def jPolyLike(pZero,dVal,fateVal,sclf):
-  # K=2
-  # nObs = obs.shape[0] ## jax cannot evaluate?
-  ## NOTE: make sure this scaling constant matches scl_fac in PolyMort
-  # pZero = 0.05*pZero
-  # print(f"{pZero=}")
+
   pZero = sclf*pZero
-  pZero = jnp.array(pZero)
+  # pZero = jnp.array(pZero)
+
   s0 = 1 - jnp.sum(pZero)
   m2 = 1 - s0 - pZero[0] # won't be used anyway if len(pVal) = 1
   arr = [[s0,0,0],[pZero[0],1,0],[m2,0,1]]
   M = jnp.array(arr,dtype=jnp.float64) # print(f"\t\t{mat=}") 
-  NLL = 0 ## initial NLL value
 
+  NLL = 0 ## initial NLL value
   NLL = nll_lookup(M,dVal,fateVal)
 
-  # M2 = M @ M
-  # M3 = M2 @ M 
-  # M4 = M2 @ M2
-  # M5 = M3 @ M2 
-  # M6 = M3 @ M3
-
-  # Mpower = jnp.array([M,M2,M3,M4,M5,M6])
-  # ## this is actually MUCH slower
-  # LL = Mpower[dVal,fateVal,0]
-  # NLL = -sum(jnp.log(LL)) # print(f"{Mpower=}")
-  # print(f"jPolyLike: {type(d)=} {d.dtype=} {np.unique(d,return_counts=True)=}"
-        # f"\n\t {type(fate)=} {fate.dtype=} {np.unique(fate,return_counts=True)=}")
-
-  ## the problem is still that the matrix power cannot easily be vectorized
-  # for n in jnp.arange(nObs):
-  #   # d = int(obs[n,1]) # fate = int(obs[n,2])
-  #   d = dVal[n]
-  #   fate = fateVal[n]
-  #   M_to_the_d = jnp.linalg.matrix_power(M,d)  ## doesn't work w/tracers
-  #   L = M_to_the_d[fate,0] # L = M_to_the_d[fate-1,0]
-  #   NLL = NLL - jnp.log(L + 1e-15)
-    # L = Mpower[d,fate,0]
-    # def matpow(i,mat): return jnp.dot(mat,M)
-    # ## creates the identity matrix and multiplies it by M, d times
-    # ## incrementally changes value (see jax.lax.fori_loop documentation
-    # M_to_the_d = jax.lax.fori_loop(0,d,matpow,jnp.eye(3))
-
-    ## will print when atype==small2 (which makes debugLL=4)
-    # if config.debugLL>=4: print(f"\t\tnest ID: {obs[n,0]}",end=" ")
-    # if config.debugLL>=4: print(f" {fate=} {d=}")
-    # print(f" {M_to_the_d=}",end=" ")
-    # if config.debugLL>=4: print(f"\t\tM_to_the_d[fate-1,0] {-jnp.log(L)=} {L=}")
-
-  # if config.debugLL>=4: print(f"\tjPolyLike: {NLL=} {type(NLL)=}")
   return NLL
 
 def jplWrapper(pZero,dVals,fates,sclf):
-  # if config.debugLL>=4: print(f"\t {type(M)=} {M.dtype=} {M.shape=}\n{M.flatten()=}")
   ## Calls jPolyLike thru ll_valgrad to get val & gradient
   loss, grad = ll_valgrad(pZero,dVals,fates,sclf)
   # print(f"{val=} {type(val)=}")
@@ -124,51 +90,48 @@ def jplWrapper(pZero,dVals,fates,sclf):
 ll_valgrad = jax.value_and_grad(jPolyLike)
 
 # def PolyMort(obsData,survey,config,useJax=False,scl_fac=0.1,plt=True,suff=""):
-def PolyMort(obsData,survey,config,useJax=True,scl_fac=0.05,plt=False,suff=""):
+def PolyMort(obsData,survey,par,config,useJax=True,scl_fac=0.05,plt=False,suff=""):
 
+  # obsData = jnp.array(obsData.to_numpy())
+  # dVal = obsData[:,1].astype(jnp.int64)
+  # fateVal = obsData[:,2].astype(jnp.int64)
   obs = mk_obs_mat(obsData,survey,config)
   dVal = obs[:,1].astype(jnp.int64)
   fateVal = obs[:,2].astype(jnp.int64)
+  nObs = int(obs.shape[0])
+  outLen = 1300 if par.numNests==250 else 2400
+  pWidth = ((0,outLen-nObs))
+  # dVal = jnp.pad(dVal,pad_width=pWidth,constant_values=-99) ## should pad w/-99
+  dVal = jnp.pad(dVal,pad_width=pWidth) ## should pad w/zeros
+  fateVal = jnp.pad(fateVal,pad_width=pWidth) ## should pad w/zeros
+  ## padding with 0 gets correct index to make mPowFate == 1 so log = 0
+  # fateVal = jnp.pad(fateVal,pad_width=pWidth,constant_values=-2) ## should pad w/zeros
+  # print(dVal)
 
   K = len(np.unique(obs[:,2])) - 1 ## number of different fates minus 1
   gtolr = 1e-6
-  ftolr = 1e-6
-  # opt = {'gtol':gtolr,'ftol':ftolr,'disp':True}
+  ftolr = 1e-6 # opt = {'gtol':gtolr,'ftol':ftolr,'disp':True}
   opt = {'disp':True}
-  fun = jPolyLike
-  # met = config.optimizer
+  # fun1 = jPolyLike # met = config.optimizer
   met = "L-BFGS-B"
-  nObs = int(obs.shape[0])
   # arg = (obs,nObs,scl_fac) ## extra args to pass to jplWrapper
   arg = (dVal,fateVal,scl_fac) ## extra args to pass to jplWrapper
   lb, ub = 0.001, 1.0
   bnd = optimize.Bounds(lb,ub)
-  # ll_obj =  functools.partial(fun,obs=obs,nObs =nObs)
-  # ll_obj =  functools.partial(fun,obs=obs,nObs =nObs,scl_fac=scl_fac)
-
-  if useJax:
-    fun = jplWrapper
-    jaco = True 
-    # prob = Optimized(obs=obs)
-    # fun = prob.objective
-    # jaco = prob.gradient
-  else:
-    fun = PolyLikelihood
-    ll_obj =  functools.partial(fun,obs=obs,K=K,sclf=scl_fac)
-    jaco=False # jaco = jax.grad(fun)
-
-  pZero = rng.uniform(low=0.2,high=0.7,size=(K)) # print(f"untransformed {pZero=}")
-  s0 = 1 - np.sum(pZero)
-  # print(f"\n\t>> PolyMort: {K=} {nObs=} {type(pZero)=} {pZero.dtype=} {pZero.shape=}")
-  # print(f"\t\t {type(obs)=} {obs.dtype=} {obs.shape=} ")
-
-  ## NOTE specifying scl_fac inside of jPolyLike insteead of as arg
-  # arg = (obs,nObs,)
+  # ll_obj =  functools.partial(fun1,obs=obs,nObs =nObs,scl_fac=scl_fac)
+  fun = jplWrapper 
+  jaco = True 
+  pZero = rng.uniform(low=0.2,high=0.7,size=(K)) 
 
   if plt: plot_jac(fun,arg,suff)
+
+  # print(f"\n\t>> PolyMort: {K=} {nObs=} {type(pZero)=} {pZero.dtype=} {pZero.shape=}")
+  # print(f"\t\t {type(obs)=} {obs.dtype=} {obs.shape=} ")
+  ## NOTE specifying scl_fac inside of jPolyLike insteead of as arg
   # print(f"\t>> PolyMort: run optimizer - {met=} {gtolr=}; untransformed {pZero=}\n{fun=} {bnd=}")
   ## NOTE can use keep_feasible w/trust-constr to stay w/in bounds throughout
   ## NOTE don't need to pass arg if obs is in the class instance
+
   out = optimize.minimize(fun,
                           pZero,
                           args=arg,
@@ -182,13 +145,13 @@ def PolyMort(obsData,survey,config,useJax=True,scl_fac=0.05,plt=False,suff=""):
                           # options={'disp':True},
                  )
 
+  hess = np.asarray(jax.hessian(jPolyLike)(out.x,*arg),dtype=np.float64)
+  se   = np.sqrt(np.diag(np.linalg.inv(hess))) * scl_fac
+  ans  = out.x * scl_fac
+  s    = 1-sum(ans) ## one minus sum of fitted values
   # print(f"\t>> PolyMort: {out.success=} {out.message=} {out.nit=} {out.nfev=}")
   # print(f"*** PolyMort: {ans.dtype=} {ans.shape=} {ans=} {s=} ",end=" ")
-  # hess      = np.asarray(jax.hessian(jPolyLike)(out.x,obs,nObs,scl_fac),dtype=np.float64)
-  hess      = np.asarray(jax.hessian(jPolyLike)(out.x,*arg),dtype=np.float64)
-  se = np.sqrt(np.diag(np.linalg.inv(hess))) * scl_fac
-  ans = out.x * scl_fac
-  s = 1-sum(ans) ## one minus sum of fitted values
+
   ## this was summing along each axis
   # seS = np.sqrt(np.sum(np.sum(np.linalg.inv(hess)))) * scl_fac
   # seS = np.sqrt(np.sum(np.linalg.inv(hess))) * scl_fac
